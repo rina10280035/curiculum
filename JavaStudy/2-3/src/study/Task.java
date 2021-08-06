03114d4620eb8e488912208737e7a0118f27e988
rina10280035 <squall_04ls.0035@docomo.ne.jp>
2021-08-06 23:27:02 +0900

    Update Task.java

--- JavaStudy/2-3/src/study/Task.java
+++ JavaStudy/2-3/src/study/Task.java
@@ -18,8 +18,8 @@ public void doTask() {
 	
 
 	System.out.println("plusメソッドの引数が一つの場合:" + 11);
-	System.out.println("plusメソッドの引数が一つの場合:" + 30);
-	System.out.println("plusメソッドの引数が一つの場合:" + 60);
+	System.out.println("plusメソッドの引数が二つの場合:" + 30);
+	System.out.println("plusメソッドの引数が三つの場合:" + 60);
 	
 
 }
