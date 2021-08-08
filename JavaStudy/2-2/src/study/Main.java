2f9216c6ea4f0ff275e95cd249e14bc76293a5d8
rina10280035 <squall_04ls.0035@docomo.ne.jp>
2021-08-08 21:18:30 +0900

    課題提出(再作成)
    2－2の再作成

--- JavaStudy/2-2/src/study/Main.java
+++ JavaStudy/2-2/src/study/Main.java
@@ -17,13 +17,12 @@ public class Main {
 		// TODO 自動生成されたメソッド・スタブ
 		  // ① 以下のルールに従いUserクラスのインスタンスを生成してください。
         // 変数名:「user」
-		User user = new User("hoge", 123 , ""); 
+		User user = new User("hoge", 123, "abc123"); 
+		
+	
 
         // ② ①のインスタンス変数より、アカウント情報をコンソール出力するメソッドを呼び出しなさい。
 		user.printAccountInfo();
-		user.printAccountInfo1();
-		user.printAccountInfo2();
-
     }
 
 
--- JavaStudy/2-2/src/study/User.java
+++ JavaStudy/2-2/src/study/User.java
@@ -16,35 +16,16 @@ public class User {
         this.password = "abc123";
     }
 
-
-		// TODO 自動生成されたコンストラクター・スタブ
-
-
-	public User() {
-		// TODO 自動生成されたコンストラクター・スタブ
-	}
-
-
 	// ① 以下のルールに従いアカウント情報（名前、ID、パスワード）を出力するメソッドを作成してください。
     // アクセス修飾子:「protected」
     // メソッド名:「printAccountInfo」
     protected void printAccountInfo() {
     	System.out.println("名前は、" + this.userName);
-    }
-    protected void printAccountInfo1() {
     	System.out.println("IDは、" + this.id);
-    }
-    protected void printAccountInfo2() {
     	System.out.println("パスワードは、" +  this.password);
     }
+ }
 
-
-		
-	
-
-
-	
-	}
 
 
 
