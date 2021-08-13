3fd3811c8defa2b07bb2a08c43771eefdc34ee2a
rina10280035 <squall_04ls.0035@docomo.ne.jp>
2021-08-13 19:31:49 +0900

    2-6課題訂正

--- JavaStudy/2-6/src/study/Study.java
+++ JavaStudy/2-6/src/study/Study.java
@@ -29,7 +29,7 @@ public class Study {
 
         // 問② 「(Calendar.MONTH) + 数値」の処理は、想定していない値となる可能性があります。
         // その理由をコメントへ記述してください。
-        // [ここへ記述]月の値は、「-1」の値を保持している為
+        // [ここへ記述]あくまで「月の値+1」である為、年の加算減算してくれない為。
         System.out.println(calendar.get(Calendar.YEAR));
         System.out.println(calendar.get(Calendar.MONTH) + 1);
         System.out.println(calendar.get(Calendar.DATE));
