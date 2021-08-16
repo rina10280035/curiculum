a91a13d7638342d0d5c3b6c37b53dc599e287567
rina10280035 <squall_04ls.0035@docomo.ne.jp>
2021-08-16 13:04:31 +0900

    Check2 課題訂正

--- JavaStudy/Check2/src/check/Check.java
+++ JavaStudy/Check2/src/check/Check.java
@@ -3,19 +3,19 @@ package check;
 
 public class Check {
 	
-	
+	public static final String CHECK_CLASS_JAVA = "java吉";
+    public static final String CHECK_CLASS_HOGE = "hoge";
+    public static final String CHECK_CLASS_R2D2 = "R2D2";
+    public static final String CHECK_CLASS_LUKE = "ルーク";
+
 	
 	public static void main(String[] args) {
 		Check printName = new Check("さとう","りな");
 		printName.printName();
 		
-		
-		String CHECK_CLASS_JAVA = "java吉";
-		String CHECK_CLASS_HOGE = "hoge";
 		Pet introduce = new Pet(CHECK_CLASS_JAVA,CHECK_CLASS_HOGE);
 		
-        String CHECK_CLASS_R2D2 = "R2D2";
-		String CHECK_CLASS_LUKE = "ルーク";
+        
 		RobotPet pet = new RobotPet(CHECK_CLASS_R2D2,CHECK_CLASS_LUKE);
          
 		introduce.introduce();
