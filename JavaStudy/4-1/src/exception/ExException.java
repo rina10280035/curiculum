d4692df802032ead37d135c8d483f588964ff1a7
rina10280035 <squall_04ls.0035@docomo.ne.jp>
2021-09-13 22:05:38 +0900

    4-1　課題訂正

--- JavaStudy/4-1/src/exception/ExException.java
+++ JavaStudy/4-1/src/exception/ExException.java
@@ -69,10 +69,12 @@ public class ExException {
                     
                     case CONST_EXCEPTION_TRIGER_ARRAY_OUT_OF_BOUNDS:
                     // 問②: 「throw」を使用せずに「ArrayIndexOutOfBoundsException」を発生させる処理を記述しなさい。
-                    	new ArrayIndexOutOfBoundsException();
+                    
                     // Tips: ご自身で配列を準備してください（使用する配列の型、要素数は自由）
                     // ここへ記述
-                    	int[] IntArray =  { 1 , 2 , 3 };
+                    	int[] IntArray =  { 1 , 2 , 3 , 4};
+                    	
+                    	System.out.println(IntArray[4]);
                     break;
                     	
                     case CONST_EXCEPTION_TRIGER_CAST:
